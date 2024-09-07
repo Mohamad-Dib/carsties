dc816e97d358b2dbe30c5c131d5f693af3e2b14b
Mohamad-Dib <moha2004.md@gmail.com>
2024-09-07 13:17:55 +0300

    End of section 2

---
+++ src/AuctionService/Data/Migrations/20240830131121_InitialCreate1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURl",
+                table: "Items",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Items",
+                newName: "ImageURl");
+        }
+    }
+}
